32195a84599bc398dab6c894012b382896dac29d
igorczarr <igorczarr@gmail.com>
2025-05-16 01:59:52 -0300

    feat: Companies and Locations list
    Ref: #3

--- frontend/src/views/pages/system/CompaniesPage.vue
+++ frontend/src/views/pages/system/CompaniesPage.vue
@@ -143,4 +143,113 @@
         filters.value = {name: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] }};
     }
 
+{
+  "locations"; [
+    {
+      "id": "loc-001",
+      "name": "São Paulo Office",
+      "address": "Av. Paulista, 1000, São Paulo - SP"
+    },
+    {
+      "id": "loc-002",
+      "name": "Rio de Janeiro Hub",
+      "address": "Rua das Laranjeiras, 45, Rio de Janeiro - RJ"
+    },
+    {
+      "id": "loc-003",
+      "name": "Belo Horizonte Branch",
+      "address": "Av. Afonso Pena, 500, Belo Horizonte - MG"
+    },
+    {
+      "id": "loc-004",
+      "name": "Curitiba Base",
+      "address": "Rua XV de Novembro, 1234, Curitiba - PR"
+    },
+    {
+      "id": "loc-005",
+      "name": "Salvador Point",
+      "address": "Av. Tancredo Neves, 2500, Salvador - BA"
+    }
+  ],
+  
+  "companies"; [
+    {
+      "id": "comp-001",
+      "name": "Alpha Tech",
+      "location_id": "loc-001"
+    },
+    {
+      "id": "comp-002",
+      "name": "Beta Corp",
+      "location_id": "loc-002"
+    },
+    {
+      "id": "comp-003",
+      "name": "Gamma Solutions",
+      "location_id": "loc-003"
+    },
+    {
+      "id": "comp-004",
+      "name": "Delta Group",
+      "location_id": "loc-004"
+    },
+    {
+      "id": "comp-005",
+      "name": "Epsilon Systems",
+      "location_id": "loc-005"
+    },
+    {
+      "id": "comp-006",
+      "name": "Zeta Innovations",
+      "location_id": "loc-001"
+    },
+    {
+      "id": "comp-007",
+      "name": "Eta Enterprises",
+      "location_id": "loc-002"
+    },
+    {
+      "id": "comp-008",
+      "name": "Theta Ventures",
+      "location_id": "loc-003"
+    },
+    {
+      "id": "comp-009",
+      "name": "Iota Networks",
+      "location_id": "loc-004"
+    },
+    {
+      "id": "comp-010",
+      "name": "Kappa Logistics",
+      "location_id": "loc-005"
+    },
+    {
+      "id": "comp-011",
+      "name": "Lambda Energy",
+      "location_id": "loc-001"
+    },
+    {
+      "id": "comp-012",
+      "name": "Mu Finance",
+      "location_id": "loc-002"
+    },
+    {
+      "id": "comp-013",
+      "name": "Nu Robotics",
+      "location_id": "loc-003"
+    },
+    {
+      "id": "comp-014",
+      "name": "Xi Biotech",
+      "location_id": "loc-004"
+    },
+    {
+      "id": "comp-015",
+      "name": "Omicron Foods",
+      "location_id": "loc-005"
+    }
+  ]
+}
+
 </script>
+   
