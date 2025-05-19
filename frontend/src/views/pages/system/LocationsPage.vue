2a1a820c698306ba6cf753c86ebe889143058b86
DevMiguelReis <miguelnovais55@gmail.com>
2025-05-19 15:53:51 -0300

    feat: codigo deletado
    Ref: #1

--- frontend/src/views/pages/system/LocationsPage.vue
+++ frontend/src/views/pages/system/LocationsPage.vue
@@ -1,15 +1,5 @@
 <template>
-    <div class="font-semibold text-xl">Severities</div>
-    <div class="flex flex-wrap gap-2">
-        <Button label="Primary" />
-        <Button label="Secondary" severity="secondary" />
-        <Button label="Success" severity="success" />
-        <Button label="Info" severity="info" />
-        <Button label="Warn" severity="warn" />
-        <Button label="Help" severity="help" />
-        <Button label="Danger" severity="danger" />
-        <Button label="Contrast" severity="contrast" />
-    </div>
+    
 </template>
 
 <script setup>
